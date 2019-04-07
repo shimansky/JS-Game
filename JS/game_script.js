def6ac0cde658731b281f91b12f22b805a303660
Igor Shimanskyi <shima3d@gmail.com>
2019-04-07 12:25:15 +0300

    Добавил обработчики столкновений, взятия карточек, открытия дверей. Анимировал бота.

--- JS/game_script.js
+++ JS/game_script.js
@@ -2,9 +2,48 @@
 var left=400;
 var bottom=150;
 var box;
+var temp = 100;
+var vertical=450;
+
+
+// блок функций анимации бота
+ var botMoveRight = function() {
+  temp=temp+50;
+  enemy.style.left = temp + 'px';
+  
+}
+
+ var botMoveLeft = function() {
+  temp=temp-50;
+  enemy.style.left = temp + 'px';
+  
+}
+
+var botMoveUp = function() {
+  vertical=vertical+50;
+  enemy.style.bottom = vertical + 'px';
+  
+}
+
+var botMoveDown = function() {
+  vertical=vertical-50;
+  enemy.style.bottom = vertical + 'px';
+
+}
+
+function botMove(){
+setTimeout(botMoveRight, 500);
+setTimeout(botMoveRight, 1000);
+setTimeout(botMoveLeft, 1500);
+setTimeout(botMoveLeft, 2000);
+
+}
+
+
+
 // функция, выполняется при загрузке - находит элементы по id присваивает им имена
 window.onload = function(){
-alert("\"HIDDEN STRUCTURE\" you got somewhere in an unknown place... ")
+// alert("\"HIDDEN STRUCTURE\" you got somewhere in an unknown place... ")
 figure=document.getElementById('player');
 keywhite=document.getElementById('key_card_white');
 keyblack=document.getElementById('key_card_black');
@@ -12,6 +51,8 @@ enemy=document.getElementById('monster');
 lock_white=document.getElementById('locked_door_white');
 lock_black=document.getElementById('locked_door_black');
 exit_door=document.getElementById('exit_door_open');
+setInterval(get_coordinates,10);
+setInterval(botMove,2000);
  };
 
 
@@ -23,21 +64,24 @@ var m = enemy.getBoundingClientRect();
 var k = keywhite.getBoundingClientRect();
 var b = keyblack.getBoundingClientRect();
 
+// логика работы дверей и карточек-ключей
 if (p.right==k.right&&p.bottom==k.bottom) {
-    alert("you have a white key!");
+    // alert("you have a white key!");
     lock_white.id="unlocked_door";
     keywhite.style.display="none";
   }
 
+
 if (p.right==b.right&&p.bottom==b.bottom) {
-    alert("you have a black key!");
+    // alert("you have a black key!");
     lock_black.id="unlocked_door";
     keyblack.style.display="none";
   }
 
+// отслеживание столкновений с ботом
+ if (p.right==m.right&&p.bottom==m.bottom){
+  alert("monster detected!");
 
-if (p.right==m.right&&p.bottom==m.bottom){
-  alert("monster detected!"); 
 }
 
  
@@ -73,10 +117,7 @@ up_empty = up_elem.id;
 
 }
 
-// сценарий поведения бота
-function bot_scenery(){
 
-}
 
 // обаботчики нажатий на клавиши
 window.onkeyup = function(){
@@ -85,16 +126,18 @@ window.onkeyup = function(){
  // left
  if(event.keyCode==37){
     get_coordinates();
+    
     if (left_empty!="wall"&&left_empty!="locked_door_white"&&left_empty!="locked_door_black") {
     left=left-50;
     figure.style.left = left + 'px';
     figure.style.backgroundImage='URL("IMG/player_l.png")';
     }
+}
 
-  }
   // right
     else if(event.keyCode==39){
       get_coordinates();
+     
       if (right_empty!="wall"&&right_empty!="locked_door_white"&&right_empty!="locked_door_black") {
       left=left+50;
       figure.style.left= left + 'px';
@@ -105,75 +148,66 @@ window.onkeyup = function(){
    // down
     else if(event.keyCode==40){
       get_coordinates();
+      
+      if (down_empty!="wall"&&down_empty!="locked_door_white"&&down_empty!="locked_door_black") {
+      bottom=bottom-50;
+      figure.style.bottom= bottom + 'px';
+      figure.style.backgroundImage='URL("IMG/player_r.png")';
+      }
+}
+
+// up
+    else if(event.keyCode==38){
+      get_coordinates();
+     
+      if (up_empty!="wall"&&up_empty!="locked_door_white"&&up_empty!="locked_door_black") {
+      bottom=bottom+50;
+      figure.style.bottom= bottom + 'px';
+      figure.style.backgroundImage='URL("IMG/player_l.png")';
+     }
+}  
+
+}
+  
+
+
+// обаботчики нажатия кнопок на станице
+function up(){
+   get_coordinates();
+      if (up_empty!="wall"&&up_empty!="locked_door_white"&&up_empty!="locked_door_black") {
+      bottom=bottom+50;
+      figure.style.bottom= bottom + 'px';
+      figure.style.backgroundImage='URL("IMG/player_l.png")';
+     }
+	
+}
+
+function down(){
+  get_coordinates();
       if (down_empty!="wall"&&down_empty!="locked_door_white"&&down_empty!="locked_door_black") {
       bottom=bottom-50;
       figure.style.bottom= bottom + 'px';
       figure.style.backgroundImage='URL("IMG/player_r.png")';
       
      }
-    
-    }
-// up
-    else if(event.keyCode==38){
-      get_coordinates();
-      if (up_empty!="wall"&&up_empty!="locked_door_white"&&up_empty!="locked_door_black") {
-      bottom=bottom+50;
-      figure.style.bottom= bottom + 'px';
-      figure.style.backgroundImage='URL("IMG/player_l.png")';
-     }
-     }  
-    }
-  
-
-
-// обаботчики нажатия кнопок на станице
-function up(){
-  bottom=bottom+50;
-      figure.style.bottom= bottom + 'px';
-      figure.style.backgroundImage='URL("IMG/player_l.png")';
-  var p = figure.getBoundingClientRect();
-  var m = enemy.getBoundingClientRect();
-  console.log("player position:" + p.right)
-  if (p.right-m.left<1){
-    alert("collision detected!"); 
-};
-	
-}
-
-function down(){
-  bottom=bottom-50;
-      figure.style.bottom= bottom + 'px';
-      figure.style.backgroundImage='URL("IMG/player_r.png")';
-  var p = figure.getBoundingClientRect();
-  var m = enemy.getBoundingClientRect();
-  console.log("player position:" + p.right)
-  if (p.right-m.left<1){
-    alert("collision detected!"); 
-};
 }
 
 function lefty(){
-  left=left-50;
+  get_coordinates();
+    if (left_empty!="wall"&&left_empty!="locked_door_white"&&left_empty!="locked_door_black") {
+    left=left-50;
     figure.style.left = left + 'px';
     figure.style.backgroundImage='URL("IMG/player_l.png")';
-  var p = figure.getBoundingClientRect();
-  var m = enemy.getBoundingClientRect();
-  console.log("player position:" + p.right)
-  if (p.right-m.left<1){
-    alert("collision detected!"); 
-};
+    }
 	
 }
 
 function right(){
-  left=left+50;
+   get_coordinates();
+      if (right_empty!="wall"&&right_empty!="locked_door_white"&&right_empty!="locked_door_black") {
+      left=left+50;
       figure.style.left= left + 'px';
       figure.style.backgroundImage='URL("IMG/player_r.png")';
-  var p = figure.getBoundingClientRect();
-  var m = enemy.getBoundingClientRect();
-  console.log("player position:" + p.right)
-  if (p.right-m.left<1){
-    alert("collision detected!"); 
-};
+      }
 	
 }
